0992feb2346bfdb093a17ca78a88453653f3c52b
darynazin <danayu@ukr.net>
2025-02-10 18:41:56 +0100

    added leaderboard page and roster page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import MyRoster from "./pages/MyRoster";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/pokemon/:name" element={<PokemonDetails />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/roster" element={<MyRoster />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
       <Footer />
     </Router>
