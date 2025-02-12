1ef4cb49b572850926509798f9d96cc9081ce2aa
Jayalakshmi <p.jayalakshmi3@gmail.com>
2025-02-12 14:10:22 +0100

    fifthcommit

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -59,7 +59,7 @@ function PokemonDetails() {
     return <div className="text-center p-6">Pokemon not found!</div>;
 
   return (
-    <div className="skeleton max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-md text-black">
+    <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-md text-black">
       <h1 className="text-4xl font-extrabold text-center text-gray-800 mb-6 capitalize">
         {pokemon.name}
       </h1>
